4e6f1b78982dbd4085dc9acd5a9e9d0e1874e4b3
Willian Rocha <willian.rocha@viaflow.com.br>
2020-02-03 16:43:23 -0300

    final

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-require('dotenv');
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 const routes = require('../src/routes');
-const PORT = process.env || 3333;
+const PORT = process.env.PORT || 3333;
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
